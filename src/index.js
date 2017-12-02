ee762a5d798defb84d7c70734b708e5dbd540a19
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-12-02 20:19:34 +0300

    Set up app with babel and webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const name = 'Vova';
+console.log(`Hello, ${name}`);
---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
